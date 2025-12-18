28f5d0df71f10c8fbe96cd018504b9e4970d28b7
m-heisters <marcus.heisters@googlemail.com>
2025-12-18 20:32:23 +0000

    implement exit command

--- repl.go
+++ repl.go
@@ -7,21 +7,47 @@ import (
 	"strings"
 )
 
-func cleanInput(text string) []string {
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
+var supportedCommands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the pokedex",
+		callback:    commandExit,
+	},
+}
+
+func cleanInput(text string) []string {
 	trimmed := strings.TrimSpace(strings.ToLower(text))
 	return strings.Split(trimmed, " ")
 }
 
-func StartRepl() {
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
 
+func StartRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("pokedex > ")
 		scanner.Scan()
 		input := scanner.Text()
-		firstWord := cleanInput(input)[0]
-		fmt.Printf("Your command was: %v\n", firstWord)
+		userCommand := cleanInput(input)[0]
+		command, ok := supportedCommands[userCommand]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println("Something went wrong")
+		}
 	}
 
 }
