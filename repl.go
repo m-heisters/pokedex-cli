517201583cd9d34b2d2a23cc894d8a0638534f9e
m-heisters <marcus.heisters@googlemail.com>
2025-12-18 19:57:51 +0000

    implement basic repl

--- repl.go
+++ repl.go
@@ -1,9 +1,27 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 
-	trimmed := strings.TrimSpace(text)
+	trimmed := strings.TrimSpace(strings.ToLower(text))
 	return strings.Split(trimmed, " ")
+}
+
+func StartRepl() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		firstWord := cleanInput(input)[0]
+		fmt.Printf("Your command was: %v\n", firstWord)
+	}
+
 }
