72c9c1b27727a8b065a3d63d63c85ae49b391b02
m-heisters <marcus.heisters@googlemail.com>
2025-12-23 21:51:07 +0000

    refactoring according to the course to make it more readable and in go style

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/m-heisters/pokedex-cli/internal/pokeapi"
+	"time"
+)
+
 func main() {
-	StartRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+	startRepl(cfg)
 }
--- repl.go
+++ repl.go
@@ -5,29 +5,67 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/m-heisters/pokedex-cli/internal/pokeapi"
 )
+
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
 
 func cleanInput(text string) []string {
 	trimmed := strings.TrimSpace(strings.ToLower(text))
 	return strings.Split(trimmed, " ")
 }
 
-func StartRepl() {
+func startRepl(cfg *config) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("pokedex > ")
 		scanner.Scan()
 		input := scanner.Text()
 		userCommand := cleanInput(input)[0]
-		command, ok := supportedCommands[userCommand]
+		command, ok := getCommands()[userCommand]
 		if !ok {
 			fmt.Println("Unknown command")
 			continue
 		}
-		err := command.callback()
+		err := command.callback(cfg)
 		if err != nil {
 			fmt.Println("Something went wrong")
 		}
 	}
+}
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*config) error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
